0616010c0b221d815261f28f2c65e54e9f980b4d
imnotnoahhh <qinfuyaoo@icloud.com>
2026-03-01 22:56:12 +0800

    style: run cargo fmt

--- src/error.rs
+++ src/error.rs
@@ -126,7 +126,9 @@ mod tests {
     #[test]
     fn test_error_display_home_directory_not_found() {
         let err = VexError::HomeDirectoryNotFound;
-        assert!(err.to_string().contains("Could not determine home directory"));
+        assert!(err
+            .to_string()
+            .contains("Could not determine home directory"));
         assert!(err.to_string().contains("HOME environment variable"));
     }
 }
